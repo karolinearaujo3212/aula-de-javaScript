879f925aad6d411aa4774cf6f25dfef79f4ea00d
karoline araujo <karo.linearaujo1232@gmail.com>
2025-04-03 11:30:09 -0400

    aula1java

---
+++ aula1.js
@@ -0,0 +1,41 @@
+var nome = "joao";
+let idade = 25
+const PI = 3.14; 
+
+let numero = 42; 
+let texto = "isso e uma string";
+let isVerdade = true; 
+let frutas = ["maca", "banana", "laranja"];
+console.log (nome)
+
+let uva = 10
+let morango = 5 
+let resultado 
+
+resultado = uva + morango 
+
+console.log (resultado)
+
+let kiwi = 10
+let tangirina = 5
+let resolucao
+
+resolucao = kiwi * tangirina
+
+console.log (resolucao)
+
+let limao = 10
+let amora = 5 
+let resposta 
+
+resposta = limao - amora 
+
+console.log (resposta)
+
+let maracuja = 10 
+let manga = 5 
+let solucao 
+
+solucao = maracuja / manga 
+
+console.log (solucao)
