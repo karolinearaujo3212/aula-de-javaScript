95d59bcccb9e6e35931eb945f55bb278c34f63fe
karoline araujo <karo.linearaujo1232@gmail.com>
2025-04-10 11:15:45 -0400

    aula2.javascript

--- aula2.6.js
+++ aula2.6.js
@@ -1,5 +1,3 @@
 var fahrenheit = 77 
-let resultado = fahrenheit - 32
-let resultado2= resultado*5/9
-let resultado3 = resultado2*5/9
-console.log (" a temperatura em celsius e", resultado2)
+let resultado = (fahrenheit - 32) * (5/9)
+console.log (" a temperatura em celsius e", resultado)
